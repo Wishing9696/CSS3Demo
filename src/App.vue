9b4d41cdee1771f8429dc4d8da7e19ffb080e6c7
Wishing9696 <870733865@qq.com>
2019-05-17 14:08:45 +0800

    add centre.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> | 
+      <router-link to="/centre">Centre</router-link>
     <router-view/>
   </div>
 </template>
@@ -11,7 +12,7 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  
   color: #2c3e50;
 }
 </style>
---
+++ src/components/centre.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="box1">
+      <span>垂直居中1</span>
+    </div>
+    <div class="box2">
+      <span>垂直居中2</span>
+    </div>
+        <div class="box3">
+      <span>垂直居中3</span>
+    </div>
+  </div>
+</template>
+
+<style>
+.box1 {
+  text-align: center;
+}
+.box2 {
+    width: 160px; /*此width为span的边框，设置后，字体在边框最左边*/
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
